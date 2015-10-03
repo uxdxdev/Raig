209a407953bb6537cc5e714bf20cc58308748af7
David Morton <davidmortonmail@gmail.com>
2015-10-03 19:49:50 +0100

    created server stub

---
+++ raig_server/src/raig_server.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : raig_server.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
+	return 0;
+}
