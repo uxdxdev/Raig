ca22d5106fe26456cd4293b758900641532a7d2e
David Morton <davidmortonmail@gmail.com>
2015-12-19 22:33:09 +0000

    updates

--- libraig/buildLinux.sh
+++
@@ -1,5 +0,0 @@
-# Build script for Linux systems
-
-g++ -std=c++11 -c -Wall -Werror -fpic raig.cc
-g++ -std=c++11 -shared -o libs/libraig.so raig.o -L../libsocket/libs -lsocket
-sudo cp libs/libraig.so /usr/lib
--- libraig/buildOSX.sh
+++
@@ -1,5 +0,0 @@
-# Build script for OSX systems
-# pwd scripts/
-
-g++ -dynamiclib raig.cc -L../libsocket/libs -o libs/libraig.dylib -lsocket
-sudo cp libs/libraig.dylib /usr/lib
