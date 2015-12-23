43fe1a567e896f719213d339eb1dcdfbadfbf14b
David <davidmortonmail@gmail.com>
2015-12-23 19:39:54 +0000

    Changed port number to listen on to 27000 and removed dependency on libraig server initialization functions

--- src/network_manager.cc
+++ src/network_manager.cc
@@ -19,15 +19,15 @@ void NetworkManager::Init(const char* ipAddress)
 	printf("Server: initialising\n");
 
 	// Create a connection for the server
-	m_iListenSocketFileDescriptor = m_RaigAI->InitConnection(NULL, (char*)"1071", TYPE_SERVER, SOCK_STREAM);
+	m_iListenSocketFileDescriptor = m_RaigAI->InitConnection(NULL, (char*)"27000", TYPE_SERVER, SOCK_STREAM);
 
 	// Listen for incoming TCP connections and set max limit of
 	// listen queue
-	m_RaigAI->ListenForConnections(m_iListenSocketFileDescriptor, MAX_LISTEN_QUEUE_SIZE);
+	Listen(m_iListenSocketFileDescriptor, MAX_LISTEN_QUEUE_SIZE);
 
 	// Signal handler for terminated processes
 	// Only needed when forking processes
-	m_RaigAI->CreateSignalHandler();
+	Signal(SIGCHLD, (void*)SignalHandler);
 
 	// AI Manager
 	m_AIManager = new AIManager();
@@ -41,9 +41,8 @@ void NetworkManager::Start()
 
 	  // Accept all incoming TCP connections and return a file descriptor
 	  // used to communicate with the client.
-	  m_iConnfd = m_RaigAI->AcceptConnection(m_iListenSocketFileDescriptor, &m_sAddress);
-
-		printf("Accepted connection from client\n");
+	  m_iConnfd = Accept(m_iListenSocketFileDescriptor, &m_sAddress);
+	  printf("Accepted connection from client\n");
 
 	  // There was no error in AcceptGameConnection()! Woo! Create a child process
 	  // to handle game for each client
